82c1a174ee4dba0d77aed17295cfd269e568b484
aarizat <aarizatr@gmail.com>
2022-12-01 21:59:36 -0500

    Day 01 solution

---
+++ 2022/day01/solution.py
@@ -0,0 +1,30 @@
+import heapq
+
+
+def part_1(file: str) -> int:
+    with open(file, mode="r") as f:
+        maximum = accum = 0
+        for line in f:
+            if line != '\n':
+                accum += int(line)
+            else:
+                maximum =  max(accum, maximum)
+                accum = 0
+        return max(maximum, accum)
+
+
+def part_2(file: str) -> int:
+    with open(file, mode="r") as f:
+        list_accum, accum = [], 0
+        for line in f:
+            if line == '\n':
+                list_accum.append(accum)
+                accum = 0
+            else:
+                accum += int(line)
+        list_accum.append(accum)
+    return sum(heapq.nlargest(3, list_accum))
+
+
+print(part_1("input.txt"))
+print(part_2("input.txt"))
