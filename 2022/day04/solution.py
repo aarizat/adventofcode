80d871fef5bc67914c6f15bf0d3415bd1a3206ac
aarizat <aarizatr@gmail.com>
2022-12-04 22:33:10 -0500

    Make code more readable

--- 2022/day04/solution.py
+++ 2022/day04/solution.py
@@ -5,9 +5,8 @@ def part_1(file: str) -> int:
     with open(file, mode="r") as f:
         count = 0
         for line in f.read().splitlines():
-            nums = list(map(int, re.split(',|-', line)))
-            r1, r2 = nums[:2], nums[2:]
-            if (r1[0] >= r2[0] and r1[1] <= r2[1]) or (r2[0] >= r1[0] and r2[1] <= r1[1]):
+            a, b, x, y = map(int, re.split(',|-', line))
+            if (a >= x and b <= y) or (x >= a and y <= b):
                 count += 1
         return count
 
@@ -16,9 +15,8 @@ def part_2(file: str) -> int:
     with open(file, mode="r") as f:
         count = 0
         for line in f.read().splitlines():
-            nums = list(map(int, re.split(',|-', line)))
-            r1, r2 = nums[:2], nums[2:]
-            if max(r1[0], r2[0]) <= min(r1[1], r2[1]):
+            a, b, x, y = map(int, re.split(',|-', line))
+            if max(a, x) <= min(b, y):
                 count += 1
         return count
 
