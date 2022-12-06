659f12a7b01675df8a36c0a3303592ef2a9f0b80
aarizat <aarizatr@gmail.com>
2022-12-06 18:47:49 -0500

    :rocket: Day 06

---
+++ 2022/day06/solution.py
@@ -0,0 +1,15 @@
+def part_1(file) -> int:
+    text = file.readline()
+    if any(len(set(text[i:(r := i+4)])) == 4 for i in range(0, len(text))):
+        return r
+
+
+def part_2(file) -> int:
+    text = file.readline()
+    if any(len(set(text[i:(r := i+14)])) == 14 for i in range(0, len(text))):
+        return r
+
+f = open('input.txt')
+print(part_1(f)) # 1598
+f = open('input.txt')
+print(part_2(f)) # 2414
