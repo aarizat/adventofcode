18efa63c4416bfb72cde4c2fcc26aa5d322a3c58
aarizat <aarizatr@gmail.com>
2022-12-08 20:20:15 -0500

    Rename variable

--- 2022/day08/solution.py
+++ 2022/day08/solution.py
@@ -3,28 +3,28 @@ from itertools import product
 
 
 def part_1(file) -> int:
-    matrix = np.array([list(map(int, line)) for line in file.read().splitlines()])
-    rows, cols = len(matrix), len(matrix[0])
+    grid = np.array([list(map(int, line)) for line in file.read().splitlines()])
+    rows, cols = len(grid), len(grid[0])
     count = 0
     for row, col in product(range(rows), range(cols)):
-        tree = matrix[row, col]
+        tree = grid[row, col]
         count +=  any([
-            all(tree > matrix[row, :col]),  # left
-            all(tree > matrix[:row, col]),  # top
-            all(tree > matrix[row, col+1:]),  # right
-            all(tree > matrix[row+1:, col])  # down
+            all(tree > grid[row, :col]),  # left
+            all(tree > grid[:row, col]),  # top
+            all(tree > grid[row, col+1:]),  # right
+            all(tree > grid[row+1:, col])  # down
         ])
     return count
 
 
 def part_2(file) -> int:
-    matrix = np.array([list(map(int, line)) for line in file.read().splitlines()])
-    rows, cols = len(matrix), len(matrix[0])
+    grid = np.array([list(map(int, line)) for line in file.read().splitlines()])
+    rows, cols = len(grid), len(grid[0])
     distances = []
     for row, col in product(range(rows), range(cols)):
-        tree = matrix[row, col]
+        tree = grid[row, col]
         dist = 1
-        for path in (matrix[row, col-1::-1], matrix[row-1::-1, col], matrix[row, col+1:], matrix[row+1:, col]):
+        for path in (grid[row, col-1::-1], grid[row-1::-1, col], grid[row, col+1:], grid[row+1:, col]):
             count = 0
             for t in path:
                 count += 1
