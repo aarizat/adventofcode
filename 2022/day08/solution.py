bdaf09af5ab010d0a2aba39a8eddcbba349ed201
aarizat <aarizatr@gmail.com>
2022-12-08 20:10:56 -0500

    Make some tiny changes

--- 2022/day08/solution.py
+++ 2022/day08/solution.py
@@ -6,25 +6,25 @@ def part_1(file) -> int:
     matrix = np.array([list(map(int, line)) for line in file.read().splitlines()])
     rows, cols = len(matrix), len(matrix[0])
     count = 0
-    for row, col in product(range(1, rows-1), range(1, cols-1)):
+    for row, col in product(range(rows), range(cols)):
         tree = matrix[row, col]
         count +=  any([
-            all(tree > matrix[row, 0:col]),
-            all(tree > matrix[0:row, col]),
-            all(tree > matrix[row, col+1:cols]),
-            all(tree > matrix[row+1:rows, col])
+            all(tree > matrix[row, :col]),  # left
+            all(tree > matrix[:row, col]),  # top
+            all(tree > matrix[row, col+1:]),  # right
+            all(tree > matrix[row+1:, col])  # down
         ])
-    return count + 2 * (cols + rows - 2)
+    return count
 
 
 def part_2(file) -> int:
     matrix = np.array([list(map(int, line)) for line in file.read().splitlines()])
     rows, cols = len(matrix), len(matrix[0])
     distances = []
-    for row, col in product(range(1, rows-1), range(1, cols-1)):
+    for row, col in product(range(rows), range(cols)):
         tree = matrix[row, col]
         dist = 1
-        for path in (matrix[row, col-1::-1], matrix[row-1::-1, col], matrix[row, col+1:cols], matrix[row+1:rows, col]):
+        for path in (matrix[row, col-1::-1], matrix[row-1::-1, col], matrix[row, col+1:], matrix[row+1:, col]):
             count = 0
             for t in path:
                 count += 1
