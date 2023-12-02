0327134ac7275b1ed0b057941bfd7dc57af51b06
aarizat <aarizatr@gmail.com>
2023-12-02 13:03:12 +0000

    🚀 Improve logic

--- 2023/day01/solution.py
+++ 2023/day01/solution.py
@@ -1,13 +1,16 @@
+import re
+
+
 map_numbers = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
 }
 
 
@@ -23,15 +26,20 @@ def solution1():
         l_ptr, r_ptr = 0, len(line) - 1
         while not line[l_ptr].isdigit(): l_ptr += 1
         while not line[r_ptr].isdigit(): r_ptr -= 1
+        # digits = [i for i in line if i.isdigit()]
+        # total = int(digits[0] + digits[-1])
         total += int(line[l_ptr] + line[r_ptr])
     print(total)
 
 
 def solution2():
-    # TODO: complete task
     total = 0
-    for line in read_input('input.txt'):
-        ...
+    for line in read_input('input2.txt'):
+        numbers = re.findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)
+        first = map_numbers.get(numbers[0], numbers[0])
+        last = map_numbers.get(numbers[-1], numbers[-1])
+        total += int(first + last)
+    print(total)
 
 
 solution1()
